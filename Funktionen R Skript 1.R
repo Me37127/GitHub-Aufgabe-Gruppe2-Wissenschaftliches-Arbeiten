98ba0cb86d04bc4f64cfca8ee69ba054bfe82920
Me37127 <101261364+Me37127@users.noreply.github.com>
2023-02-21 12:00:20 +0100

    Update Funktionen R Skript 1.R

--- Funktionen R Skript 1.R
+++ Funktionen R Skript 1.R
@@ -5,14 +5,17 @@
 
 ## Funktion a
 
-
-
-
+dmetrische <- function(x){
+  par(mfrow=c(1,2))
+  hist(x, freq = FALSE)
+  boxplot(x)
+}
 
 ## Funktion b
 
-
-
+dkat <- function(x){
+  barplot(table(x))
+}
 
 
 ## Funktion c
